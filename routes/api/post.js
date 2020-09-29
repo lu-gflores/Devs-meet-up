e2d0b7db4bdca0e365cd6541307380f5634bc2ac
George Flores <georgeflores886@gmail.com>
2020-09-29 11:47:53 -0400

    fixed post route and added post route for Post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
+//@route POST api/posts
+//@desc creating post
+//access private
 
-//@route GEt api/profile
-//@desc Test route
-//access value for public or private
-
-router.get('/', (req, res) => res.send('Post route'));
+router.post('/', [auth, check('text', 'Text is required').not().isEmpty()], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+        const post = await newPost.save()
+        res.json(post)
+    } catch (err) {
+        console.error(err.mesage)
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router;
