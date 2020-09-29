71357c0fae7fe71ea22e4e58c62972d44acb00e3
George Flores <georgeflores886@gmail.com>
2020-09-29 13:27:37 -0400

    added route for like a post

--- routes/api/post.js
+++ routes/api/post.js
@@ -93,4 +93,23 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
+//@route PUT api/posts/like/:id
+//@desc  Like a post
+//access private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        //checking if the post has been liked by specific user
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ mgs: 'Post already liked' })
+        }
+        post.likes.unshift({ user: req.user.id })
+        await post.save()
+        res.json(post.likes)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
 module.exports = router;
