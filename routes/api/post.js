bf49e2d76fa6e47aeeeab56c2bf9d7eed45a1b50
George Flores <georgeflores886@gmail.com>
2020-09-29 12:11:44 -0400

    added delete a post route

--- routes/api/post.js
+++ routes/api/post.js
@@ -67,5 +67,30 @@ router.get('/:id', auth, async (req, res) => {
     }
 })
 
+//@route Delete api/posts/:id
+//@desc Delete post 
+//access private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post Not Found' });
+        }
+
+        //check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' })
+        }
+        await post.remove();
+        res.json({ msg: 'Post removed' })
+    } catch (err) {
+        console.error(err.mesage)
+        if (err.kind == 'ObjectId') {
+            return res.status(404).json({ msg: 'Post Not Found' });
+        }
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router;
