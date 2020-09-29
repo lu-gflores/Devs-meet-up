755900facd2850fb098c1c8b0f03d95d72c476e5
George Flores <georgeflores886@gmail.com>
2020-09-29 11:53:55 -0400

    added get route for all posts

--- routes/api/post.js
+++ routes/api/post.js
@@ -30,4 +30,18 @@ router.post('/', [auth, check('text', 'Text is required').not().isEmpty()], asyn
     }
 });
 
+//@route GET api/posts
+//@desc Get all posts
+//access private
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts)
+    } catch (err) {
+        console.error(err.mesage)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
