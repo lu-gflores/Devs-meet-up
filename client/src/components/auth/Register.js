da7e188db26c174697565e792e313a2571eda011
George Flores <georgeflores886@gmail.com>
2020-09-30 13:05:13 -0400

    added props to register.js and fixed uuid

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom'
-const Register = () => {
+import { connect } from 'react-redux'
+import { setAlert } from '../../actions/alert'
+
+const Register = (props) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -15,7 +18,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do not match')
+            props.setAlert('Passwords do not match')
         } else {
 
 
@@ -80,4 +83,4 @@ const Register = () => {
         </Fragment>
     )
 }
-export default Register
+export default connect(null, { setAlert })(Register)
