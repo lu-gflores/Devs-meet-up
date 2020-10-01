9716c7de37856ec4020d09829b5c60cdc944ffb3
George Flores <georgeflores886@gmail.com>
2020-10-01 12:50:27 -0400

    updated profile actions and reducer

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+    useEffect(() => {
+        getCurrentProfile()
+    }, [])
 
-const Dashboard = props => {
     return (
         <div>
             Dashboard
@@ -10,7 +15,13 @@ const Dashboard = props => {
 }
 
 Dashboard.propTypes = {
-
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired,
 }
 
-export default Dashboard
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+})
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
