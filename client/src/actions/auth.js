c8caa4e7fd9ee29210af91ee3180d5b4994eaee8
George Flores <georgeflores886@gmail.com>
2020-10-01 10:30:19 -0400

    created proptypes for login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -36,7 +36,7 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         })
-
+        dispatch(loadUser())
     } catch (err) {
         const errors = err.response.data.errors
 
@@ -65,6 +65,8 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_SUCCESS,
             payload: res.data
         })
+
+        dispatch(loadUser())
 
     } catch (err) {
         const errors = err.response.data.errors
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 const initialState = {
     token: localStorage.getItem('token'),
@@ -22,6 +24,7 @@ export default function (state = initialState, action) {
                 user: payload
             }
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token)
             return {
                 ...state,
@@ -31,6 +34,7 @@ export default function (state = initialState, action) {
             }
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token')
             return {
                 ...state,
