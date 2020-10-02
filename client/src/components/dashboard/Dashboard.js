55725113cab6fde7ec77c390c4cfd9603bea07ef
George Flores <georgeflores886@gmail.com>
2020-10-02 11:29:24 -0400

    list education on dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile } from '../../actions/profile'
 import Spinner from '../layout/Spinner'
 import DashboardActions from '../dashboard/DashboardActions'
 import Experience from './Experience'
+import Education from './Education'
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile()
@@ -19,6 +20,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
                 <Fragment>
                     <DashboardActions />
                     <Experience experience={profile.experience} />
+                    <Education education={profile.education} />
                 </Fragment>
                 : <Fragment>
                     <p>You have not set up your profile, plase add your information</p>
