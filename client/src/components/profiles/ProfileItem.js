42e8861d491e4e7c8552a0e0915371245f4daea5
George Flores <georgeflores886@gmail.com>
2020-10-02 13:51:08 -0400

    looping through profiles...

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProfileItem = props => {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+ProfileItem.propTypes = {
+
+}
+
+export default ProfileItem
