195b4831c0dcf7b6c31233ad63a34a1f392c238d
George Flores <georgeflores886@gmail.com>
2020-10-05 15:22:04 -0400

    added comment state in redux

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post'
 import PostItem from '../posts/PostItem'
+import CommentForm from '../post/CommentForm'
 import { Link } from 'react-router-dom'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -12,7 +13,8 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     }, [getPost])
     return loading || post === null ? <Spinner /> : <Fragment>
         <Link to='/posts' className='btn'>Back to Posts</Link>
-        <PostItem post={post} showActions={false} />
+        <PostItem post={post} showActions={false} />\
+        <CommentForm postId={post._id} />
     </Fragment>
 }
 
