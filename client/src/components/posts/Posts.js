564e9b07ac627b20e588f3a2ceb6f0b4ea78d116
George Flores <georgeflores886@gmail.com>
2020-10-05 14:50:26 -0400

    added text area for post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
 import PostItem from './PostItem'
 import Spinner from '../layout/Spinner'
-
+import PostForm from './PostForm'
 const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
         getPosts()
@@ -15,6 +15,9 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
             <p className='lead'>
                 <i className='fas fa-user'></i> Welcome to the community
             </p>
+            <PostForm />
+
+
             <div className='posts'>
                 {posts.map(post => (
                     <PostItem key={post._id} post={post} />
