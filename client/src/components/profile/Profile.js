5b9e22e9ca8860530076e036d90aaf3b1f03e19f
George Flores <georgeflores886@gmail.com>
2020-10-05 09:13:50 -0400

    rendered name, bio, and skill

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions/profile'
 import ProfileTop from './ProfileTop'
-
+import ProfileAbout from './ProfileAbout'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 
@@ -19,6 +19,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
                 <div className='profile-grid my-1'>
                     <ProfileTop profile={profile} />
+                    <ProfileAbout profile={profile} />
                 </div>
             </Fragment>}
 
