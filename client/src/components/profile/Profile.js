3fb1ea29574308cbb7267e1bf677e8d9c40580d3
George Flores <georgeflores886@gmail.com>
2020-10-05 12:29:36 -0400

    added github repos to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 
     useEffect(() => {
@@ -29,16 +30,20 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                             )))}
                         </Fragment>) : (<h4>No experience credentials</h4>)}
                     </div>
-                </div>
-                <div className="profile-edu bg-white p-2">
-                    <h2 className='text-primary'>Education</h2>
-                    {profile.education.length > 0 ? (<Fragment>
-                        {profile.education.map((education => (
-                            <ProfileEducation key={education._id} education={education} />
-                        )))}
-                    </Fragment>) : (<h4>No experience credentials</h4>)}
-                </div>
 
+                    <div className="profile-edu bg-white p-2">
+                        <h2 className='text-primary'>Education</h2>
+                        {profile.education.length > 0 ? (<Fragment>
+                            {profile.education.map((education => (
+                                <ProfileEducation key={education._id} education={education} />
+                            )))}
+                        </Fragment>) : (<h4>No Education credentials</h4>)}
+                    </div>
+
+                    {profile.githubusername && (
+                        <ProfileGithub username={profile.githubusername} />
+                    )}
+                </div>
             </Fragment>}
 
         </Fragment>
