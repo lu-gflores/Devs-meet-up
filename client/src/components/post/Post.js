0737c1166d22d0304cacede874a97c55c75c70e4
George Flores <georgeflores886@gmail.com>
2020-10-06 09:56:34 -0400

    rendered comment item in discussion

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,7 +6,7 @@ import { getPost } from '../../actions/post'
 import PostItem from '../posts/PostItem'
 import CommentForm from '../post/CommentForm'
 import { Link } from 'react-router-dom'
-
+import CommentItem from '../post/CommentItem'
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
         getPost(match.params.id)
@@ -15,6 +15,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         <Link to='/posts' className='btn'>Back to Posts</Link>
         <PostItem post={post} showActions={false} />\
         <CommentForm postId={post._id} />
+        <div className='comments'>
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
 }
 
