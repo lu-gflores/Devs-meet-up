47ebb67a043069ec529059321819a8fea8f53b29
George Flores <georgeflores886@gmail.com>
2020-10-06 15:21:10 -0400

    fixed typo for github profile and removed spinner

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -11,9 +11,9 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
 
     return <div className='profile-github'>
         <h2 className='text-primary my-1'> Github Repos</h2>
-        {repos === null ? <Spinner /> : (
+        {
             repos.map(repo => (
-                <div key={repo._id} className='repo bg-white p-1 my-1'>
+                <div key={repo.id} className='repo bg-white p-1 my-1'>
                     <div>
                         <h4>
                             <a href={repo.html_url} target='_blank' rel='noopener norefer'>
@@ -37,7 +37,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
                     </div>
                 </div>
             ))
-        )}
+        }
     </div>
 }
 
