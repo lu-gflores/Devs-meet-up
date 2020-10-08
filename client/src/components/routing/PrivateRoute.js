9ff115e63e43a645b83ee1db2e8c6b745ee55be9
George Flores <georgeflores886@gmail.com>
2020-10-08 16:15:47 -0400

    removed comments from PrivateRoute.js

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading }, ...rest }) => (
-    //if the user is not logged in, redirect them to the login page
-    //else, direct them to dashboard
-    //this keeps users that are not logged in from accessing the dashboard
     <Route {...rest} render={props => !isAuthenticated && !loading ? (
         <Redirect to='/login' />) : (
             <Component {...props} />)}
